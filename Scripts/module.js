c4e3625ac261404cb07a6d289ca10f7f4e223317
Daniel Martinez Cequiel <danimc100@gmail.com>
2019-07-31 19:41:26 +0200

    Initial commit

---
+++ Scripts/module.js
@@ -0,0 +1,65 @@
+(function(){
+
+    var Registro = function(nombre, dir, telf) {
+        this.nombre = nombre;
+        this.dir = dir;
+        this.telf = telf;
+    }
+
+    var ModelView = function () {
+        var self = this;
+
+        this.nombre = ko.observable("Daniel");
+        this.telefono = ko.observable("676470386");
+        this.combinado = ko.computed(function() {
+            return self.nombre() + ":" + self.telefono();
+        });
+
+        this.listaSencilla = ko.observable(["Canyon", "Trek", "Giant", "Pinarello"]);
+
+        var datos = [
+            new Registro("Cris", "M40", "5555"),
+            new Registro("Mila", "Ventas", "4444"),
+            new Registro("Angelica", "M40", "3333")
+        ];
+
+        this.lista = ko.observableArray(datos);
+        //this.lista = ko.observable(datos);
+
+        this.addALista = function() {
+            self.lista.push(new Registro("Corina", "Toledo", "1234"));
+        }
+
+        this.eliminar = function(item, aux) {
+            console.log(item, aux);
+            self.lista.remove(item);
+        }
+
+        this.contar = ko.computed(function() {
+            return self.lista().length;
+        });
+
+        // ---------------------------------------------------------------
+
+        this.hayDatos = ko.observableArray(undefined);
+
+        this.generaDatos = function() {
+            self.hayDatos(["Portátil", "Consola", "PC sobremesa"]);
+        }
+
+        this.eliminaDatos= function() {
+            self.hayDatos(undefined);
+        }
+
+    }
+
+    var miModelView = new ModelView();
+
+    $("#cambiaNombreBtn").on("click", function() {
+        var nombre = miModelView.nombre();
+        miModelView.nombre(nombre.toUpperCase());        
+    });
+
+    ko.applyBindings(miModelView);
+
+})();
